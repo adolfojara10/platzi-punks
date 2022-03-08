92d9497a2b0520a221a89063ebef58fe6e8b26ca
adolfojara10 <gavilanesadolfo@gmail.com>
2022-03-08 10:58:48 -0600

    Finalizacion de la creacion del ADN

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -6,14 +6,16 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
+import "./PlatziPunksDNA.sol";
 
-contract PlatziPunks is ERC721, ERC721Enumerable{
+contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA{
 
     using Counters for Counters.Counter;
 
     Counters.Counter private _idCounter;
 
     uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
 
     constructor(uint256 _maxSupply) ERC721("PlatziPunks", "PLPKS"){
         maxSupply = _maxSupply;
@@ -25,18 +27,74 @@ contract PlatziPunks is ERC721, ERC721Enumerable{
         uint256 current = _idCounter.current();
 
         require(current<maxSupply, "No platzipunks left");
+
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
+
         _safeMint(msg.sender, current);
+    }
+
+    function _baseURI() internal pure override returns(string memory){
+
+        return "https://avataaars.io/";
+
+    }
+
+    function paramsURI(uint256 _dna) internal view returns(string memory){
+
+        string memory params;
+
+        params = string(abi.encodePacked(
+            "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)           
+        ));
+
+        return string(abi.encodePacked(params,"&topType=", getTopType(_dna)));
+    }
+
+    function imageFileDNA(uint256 _dna) public view returns(string memory){
+        string memory baseURI = _baseURI();
+        string memory params = paramsURI(_dna);
+
+
+        return string(abi.encodePacked(baseURI, "?", params));
+
+
     }
 
     function tokenURI(uint256 tokenID) public view override returns(string memory){
         require(_exists(tokenID),"No existe el token");
+
+        uint256 dna = tokenDNA[tokenID];
+        string memory image = imageFileDNA(dna);
 
         string memory jsonURI = Base64.encode(
             abi.encodePacked(
                 '{ "name": "PlatziPunks #',
                 tokenID,
                 '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
-                "// TODO: Calculate image URL",
+                image,
                 '"}'
             )
         );
